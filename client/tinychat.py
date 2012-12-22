87e8d533b21228e523641c70331dbbb0ef7a873a
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2012-12-21 23:56:15 -0600

    Added welcome message and changed url format.

--- client/tinychat.py
+++ client/tinychat.py
@@ -18,16 +18,20 @@
 
 import re, os, sys, getpass, readline, curses, hashlib, httplib, urllib, urllib2, cookielib, threading, time, socket, platform, base64;
 
-if(len(sys.argv)<5):
+if(len(sys.argv)<2):
  sys.exit();
-if(len(re.findall("([\da-z]+)", sys.argv[4]))<1):
+parseurl = re.findall("(.*)\#(.*)", sys.argv[1]);
+parseurl = parseurl[0];
+chatsiteurl = parseurl[0];
+chatroomname = parseurl[1];
+if(len(re.findall("([\da-z]+)", chatroomname))<1):
  sys.exit();
 myusername = str(raw_input("User: ")).decode("utf-8");
 if(len(re.findall("([\da-z]+)", myusername))<1):
  sys.exit();
 mypass = getpass.getpass();
 mypasshash = hashlib.sha512(mypass.encode("utf-8")).hexdigest();
-chaturl = sys.argv[1]+"://"+sys.argv[2]+sys.argv[3];
+chaturl = chatsiteurl;
 chathostname = getpass.getuser()+"@"+socket.gethostname();
 chatproverinfo = ["TinyChat2k", 1, 0, 0, None];
 if(sys.platform=="win32"):
@@ -47,7 +51,7 @@ tinychat_cj = cookielib.CookieJar();
 login_opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(tinychat_cj));
 login_opener.addheaders = [("Referer", ""+chaturl+"api.php"), ("User-Agent", chatua)];
 post_data = urllib.urlencode({'username': myusername, 'userpass' : mypasshash});
-tinychattxt = login_opener.open(chaturl+"api.php?act=login&room="+sys.argv[4], post_data);
+tinychattxt = login_opener.open(chaturl+"api.php?act=login&room="+chatroomname, post_data);
 signupcheck = tinychattxt.read()[:];
 if(signupcheck=="{error:room};"):
  sys.exit();
@@ -55,7 +59,7 @@ if(signupcheck=="{error:loginuser};"):
  sys.exit();
 if(signupcheck=="{warning:newuser};"):
  post_data = urllib.urlencode({'username': myusername, 'userpass': mypass});
- tinychattxt = login_opener.open(chaturl+"api.php?act=signup&room="+sys.argv[4], post_data);
+ tinychattxt = login_opener.open(chaturl+"api.php?act=signup&room="+chatroomname, post_data);
  signupcheck = tinychattxt.read()[:];
 if(signupcheck=="{error:room};"):
  sys.exit();
@@ -77,6 +81,11 @@ inputwin = curses.newwin(win_maxy,  win_maxx, win_maxy - 4, 0);
 inputwin.clear();
 inputwin.keypad(1);
 inputwin.scrollok(True);
+curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK);
+chatwin.addstr("message: ", curses.color_pair(2));
+curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK);
+chatwin.addstr("Hello "+myusername+" welcome to chat room: "+chatroomname+"\n", curses.color_pair(3));
+chatwin.refresh();
 def getstr_prompt(txt_screen, txt_prompt): 
  curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK);
  txt_screen.addstr(txt_prompt, curses.color_pair(1));
@@ -87,7 +96,7 @@ def getnewmessages():
  global threadloopstop, refreshtime, login_opener;
  oldtimestamp = None;
  while threadloopstop==False:
-  tinychattxt = login_opener.open(chaturl+"api.php?act=view&room="+sys.argv[4]);
+  tinychattxt = login_opener.open(chaturl+"api.php?act=view&room="+chatroomname);
   chattext = tinychattxt.readlines();
   chatsize = len(chattext);
   chati = 0;
@@ -120,7 +129,7 @@ while(mymessagelc!="quit" and mymessagelc!="exit"):
  mymessagelc = mymessage.lower();
  if(mymessagelc!="quit" and mymessagelc!="exit"):
   post_data = urllib.urlencode({'message': mymessage});
-  tinychating = login_opener.open(chaturl+"api.php?act=message&room="+sys.argv[4], post_data);
+  tinychating = login_opener.open(chaturl+"api.php?act=message&room="+chatroomname, post_data);
 threadloopstop=True;
 gnm.cancel();
 curses.endwin();
